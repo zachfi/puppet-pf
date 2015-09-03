57182f88d5aff8267210fc16fbe62bcb6b294d88
Zach Leslie <zach@puppetlabs.com>
2015-09-03 09:41:21 -0700

    Add test for service inclusion

--- spec/classes/pf_spec.rb
+++ spec/classes/pf_spec.rb
@@ -3,13 +3,17 @@ require 'spec_helper'
 describe 'pf' do
   context 'on OpenBSD' do
     let(:facts) { {:kernel => 'OpenBSD'} }
+    let(:params) { { :template => 'pf/default.erb' } }
     it { should contain_class('pf') }
+    it { should_not contain_service('pf') }
 
   end
 
   context 'on FreeBSD' do
     let(:facts) { {:kernel => 'FreeBSD'} }
+    let(:params) { { :template => 'pf/default.erb' } }
     it { should contain_class('pf') }
+    it { should contain_service('pf') }
 
   end
 end
