8c3c3a31c47fdbbbe51f2a671d7aec54d02c54bf
Zach Leslie <xaque208@gmail.com>
2020-11-29 15:02:54 -0800

    Clean up after TODOne

--- functions/common_class_param_value_list.pp
+++ functions/common_class_param_value_list.pp
@@ -4,10 +4,6 @@ function pf::common_class_param_value_list(
   String $common_class_param = '',
 ) {
 
-  # TODO use the $common_class variable for the resources, but use the
-  # $class_list below to filter nodes down.  We want the data from the
-  # common_class, but we want the $class_list to filter down the nodes.
-
   pf::normalize_class_names($class_list).map |$c| {
     $nodes_query = "nodes {
       resources {
